ffade364eaeae1d12630d2a200f7b4c62bc9b63f
Hunter Parcells <hunterparcells@gmail.com>
2024-10-28 16:29:54 -0400

    feat: .env loader.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 import { RootController } from './controller/root.controller';
 import { HomeDepotController } from './controller/home-depot.controller';
@@ -8,7 +9,7 @@ import { RootService } from './provider/root.service';
 import { HomeDepotService } from './provider/home-depot.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [ConfigModule.forRoot(), HttpModule],
   controllers: [RootController, HomeDepotController],
   providers: [RootService, HomeDepotService],
 })
