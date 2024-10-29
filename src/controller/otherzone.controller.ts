f69b44c4301d0f46850324675fd020fc882053cb
Hunter Parcells <hunterparcells@gmail.com>
2024-10-29 09:07:18 -0400

    wip: Start otherzone random streaming.

--- src/controller/otherzone.controller.ts
+++ src/controller/otherzone.controller.ts
@@ -1,26 +1,36 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { firstValueFrom } from 'rxjs';
 
-import { OtherzoneService } from 'src/provider/otherzone.service';
+import { OtherzoneService } from '../provider/otherzone.service';
 
 @Controller('otherzone')
 export class OtherzoneController {
-  constructor(private readonly otherzoneService: OtherzoneService) {}
+  constructor(private readonly httpService: HttpService, private readonly otherzoneService: OtherzoneService) {}
 
   @Get('random')
-  async gotoRandomUrl(@Res() res: Response) {
+  async sendRandom(@Req() req, @Res() res: Response) {
     const randomUrl = await this.otherzoneService.getRandomUrl();
+    const request = this.httpService.get(randomUrl, { responseType: 'stream' });
+    const response = await firstValueFrom(request);
+
+    res.setHeader('Content-Disposition', 'inline');
+    res.setHeader('Content-Type', response.headers['content-type']);
+    res.setHeader('Cache-Control', 'no-cache');
+
+    console.log('Sending from', randomUrl);
     
-    res.redirect(randomUrl);
+    response.data.pipe(res);
   }
   
-  @Get('random-item')
-  async getRandomItem(): Promise<string> {
-    return await this.otherzoneService.getRandomItem();
-  }
+  // @Get('random-item')
+  // async getRandomItem(): Promise<string> {
+  //   return await this.otherzoneService.getRandomItem();
+  // }
   
-  @Get('random-url')
-  async getRandomUrl(): Promise<string> {
-    return await this.otherzoneService.getRandomUrl();
-  }
+  // @Get('random-url')
+  // async getRandomUrl(): Promise<string> {
+  //   return await this.otherzoneService.getRandomUrl();
+  // }
 }
